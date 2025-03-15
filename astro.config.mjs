cd2e73375477c21b1fd4a9cc513315ec0596c8d3
spotchi <quentin.laurent93@gmail.com>
2025-03-15 19:22:50 +0100

    Change to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://spotchi.github.io/site",
   integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
